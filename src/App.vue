715f61a9d0fcdd99ef7d18b8e30f13ef1e9426f5
ThreeStonesLee <lileihdu@163.com>
2020-07-24 16:06:31 +0800

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <!-- <editor></editor> -->
     <Chat></Chat>
   </div>
 </template>
 
 <script>
-// import Editor from './components/Editor'
 import Chat from './components/Chat'
 
 export default {
   name: 'app',
   components: {
-    // Editor
     Chat
   }
 }
